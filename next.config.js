9338365ba8da58e1aa49c0043319f9d4e9dbb019
CreateWithLevi <create.with.levi@gmail.com>
2025-03-14 10:59:19 +0000

    fix: build problem

--- next.config.js
+++ next.config.js
@@ -1,16 +1,29 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    // Ignore Deno-specific imports during Next.js build
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      os: false,
+    };
+
+    return config;
+  },
+};
 
 if (process.env.NEXT_PUBLIC_TEMPO) {
-    nextConfig["experimental"] = {
-        // NextJS 13.4.8 up to 14.1.3:
-        // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
-        // NextJS 14.1.3 to 14.2.11:
-        swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
+  nextConfig["experimental"] = {
+    // NextJS 13.4.8 up to 14.1.3:
+    // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
+    // NextJS 14.1.3 to 14.2.11:
+    swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
 
-        // NextJS 15+ (Not yet supported, coming soon)
-    }
+    // NextJS 15+ (Not yet supported, coming soon)
+  };
 }
 
 module.exports = nextConfig;
